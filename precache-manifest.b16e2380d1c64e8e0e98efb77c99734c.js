c8203c83f500baca0698d495e569bb44fd945bbe
Code Sandbox <hello@codesandbox.io>
2022-01-12 10:35:25 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b16e2380d1c64e8e0e98efb77c99734c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc49a7786733cfc251a47f4f13c57e55",
+    "url": "https://jeongyong-park.github.io/csb-604je/index.html"
+  },
+  {
+    "revision": "bc553e1a8c45244ce161",
+    "url": "https://jeongyong-park.github.io/csb-604je/static/js/2.36ad2baf.chunk.js"
+  },
+  {
+    "revision": "484f1ddc7fa350307494",
+    "url": "https://jeongyong-park.github.io/csb-604je/static/js/main.b030b633.chunk.js"
+  },
+  {
+    "revision": "40b881f51f80b4b57255",
+    "url": "https://jeongyong-park.github.io/csb-604je/static/js/runtime~main.78b11d0b.js"
+  }
+]);
